85e0404fc54bf33c8f02051cdab832a6540cf3f0
Jeremy Tieman <jotieman@microsoft.com>
2020-05-14 11:52:06 -0700

    Added env parameter for API URL

--- frontend/src/components/edit-todo.component.js
+++ frontend/src/components/edit-todo.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const API_URL = process.env.API_URL || 'http://localhost:4000/';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/';
 
 export default class EditTodo extends Component {
   constructor(props) {
--- frontend/src/components/todos-list.component.js
+++ frontend/src/components/todos-list.component.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const API_URL = process.env.API_URL || 'http://localhost:4000/';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/';
 
 const Todo = props => (
   <tr>
